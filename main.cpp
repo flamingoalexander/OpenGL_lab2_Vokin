e9e30bdd07250a41c0a0654e236a478a2dd39270
Alexandr <User@ALEXANDR>
2024-03-23 12:48:30 +0800

    create drawrectangle func

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
 #include <GLFW/glfw3.h>
+void DrawRectangle(float XfirstCord, float YfirstCord, float XsecondCord, float YsecondCord) {
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0f, 1.0f, 1.0f); glVertex2f(XfirstCord, YfirstCord);
+    glColor3f(1.0f, 1.0f, 1.0f); glVertex2f(XsecondCord, YfirstCord);
+    glColor3f(1.0f, 1.0f, 1.0f); glVertex2f(XsecondCord, YsecondCord);
+
+    glColor3f(1.0f, 1.0f, 1.0f); glVertex2f(XsecondCord, YsecondCord);
+    glColor3f(1.0f, 1.0f, 1.0f); glVertex2f(XfirstCord, YsecondCord);
+    glColor3f(1.0f, 1.0f, 1.0f); glVertex2f(XfirstCord, YfirstCord);
+    glEnd();
+}
 int main(void)
 {
     GLFWwindow* window;
@@ -23,7 +34,7 @@ int main(void)
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        //DrawRectangle(-0.5, 0.5, 0.5, -0.5);
+        DrawRectangle(-0.5, 0.5, 0.5, -0.5);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
